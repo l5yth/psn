56ab801d243b70ab20de3e11b747597bcc7f82d6
l5y <220195275+l5yth@users.noreply.github.com>
2026-03-06 22:23:58 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+# Copyright (c) 2026 l5yth
+# SPDX-License-Identifier: Apache-2.0
+{
+  description = "Terminal UI for process status navigation and control";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.callPackage ./packaging/nix {};
+      }
+    );
+}
---
+++ packaging/nix/default.nix
@@ -0,0 +1,20 @@
+# Copyright (c) 2026 l5yth
+# SPDX-License-Identifier: Apache-2.0
+{ lib, rustPlatform }:
+
+rustPlatform.buildRustPackage {
+  pname = "psn";
+  version = "0.1.2";
+
+  src = lib.cleanSource ../..;
+
+  cargoLock.lockFile = ../../Cargo.lock;
+
+  meta = with lib; {
+    description = "Terminal UI for process status navigation and control";
+    homepage = "https://github.com/l5yth/psn";
+    license = licenses.asl20;
+    mainProgram = "psn";
+    platforms = platforms.linux;
+  };
+}
